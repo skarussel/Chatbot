c306932d21b84bb4b63c1c283865704dc2ecaf4c
skarussel <58844747+skarussel@users.noreply.github.com>
2020-03-22 20:25:03 +0100

    Update DB.py

--- src/DB.py
+++ src/DB.py
@@ -1,61 +1,66 @@
-from tinydb import TinyDB,Query
+from tinydb import TinyDB, Query
+
 
 class DB():
-     """ 
-    Diese Klasse verwaltet eine TinyDB Datenbank. 
-      
-    Attributes: 
-        db (TinyDB): Eine TinyDB Datenbank, die im JSON Format verwaltet wird. 
+    """
+   Diese Klasse verwaltet eine TinyDB Datenbank.
+
+   Attributes:
+       db (TinyDB): Eine TinyDB Datenbank, die im JSON Format verwaltet wird.
+   """
+
+
+def __init__(self, db_path):
+    """
+    Der Konstruktur erzeugt eine Datenbank im JSON Format,
+    im übergebenen Pfad oder stellt eine Verbindung zu diesem her,
+    falls beim Pfad bereits ein Datenbankobjekt existiert.
     """
 
-    
-    def __init__(self, db_path):
-        """ 
-        Der Konstruktur erzeugt eine Datenbank im JSON Format,
-        im übergebenen Pfad oder stellt eine Verbindung zu diesem her,
-        falls beim Pfad bereits ein Datenbankobjekt existiert.
-        """
-        
-        self.db=TinyDB(db_path)
-        
-    def select_all(self):
-        """ 
-        Diese Funktion gibt alle Einträge der Datenbank als Liste zurück
-        """
-        
-        return self.db.all()
-    
-    def select_user(self,user_id):
-        """ 
-        Diese Funktion gibt den Eintrag eines bestimmten Users zurück
-        """
-        
-        User=Query()
-        return self.db.search(User.USER_ID == user_id)
-    
-    def insert(self, user_id,name):
-        """ 
-        Diese Funktion fügt einen neuen Eintrag in die Datenbank ein
-        Das Alter (Age) ist bisher nur ein Dummy.
-        """
-        
-        self.db.insert({'USER_ID': user_id,'USER':name,'AGE':10})
-    
-    def remove_all(self):
-        """ 
-        Diese Funktion entfernt alle Einträge aus der Datenbank
-        """
-        self.db.purge()
-        
-    
-    def user_exists(self,user_id):
-        """ 
-        Diese Funktion prüft, ob ein User in der DB existiert
-        und gibt als Ergebnis einen Bool zurück
-        """
-        
-        User=Query()
-        result = self.db.search(User.USER_ID==user_id)
-        if result:
-            return True
-        return False
+    self.db = TinyDB(db_path)
+
+
+def select_all(self):
+    """
+    Diese Funktion gibt alle Einträge der Datenbank als Liste zurück
+    """
+
+    return self.db.all()
+
+
+def select_user(self, user_id):
+    """
+    Diese Funktion gibt den Eintrag eines bestimmten Users zurück
+    """
+
+    User = Query()
+    return self.db.search(User.USER_ID == user_id)
+
+
+def insert(self, user_id, name):
+    """
+    Diese Funktion fügt einen neuen Eintrag in die Datenbank ein
+    Das Alter (Age) ist bisher nur ein Dummy.
+    """
+
+    self.db.insert({'USER_ID': user_id, 'USER': name, 'AGE': 10})
+
+
+def remove_all(self):
+    """
+    Diese Funktion entfernt alle Einträge aus der Datenbank
+    """
+    self.db.purge()
+
+
+def user_exists(self, user_id):
+    """
+    Diese Funktion prüft, ob ein User in der DB existiert
+    und gibt als Ergebnis einen Bool zurück
+    """
+
+    User = Query()
+    result = self.db.search(User.USER_ID == user_id)
+    if result:
+        return True
+    return False
